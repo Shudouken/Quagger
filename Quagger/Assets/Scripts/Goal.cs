9b0d974fbfdb73b6e61b052381320705fb4446e2
Patrick Hipp <patrick.hipp@student.tugraz.at>
2017-07-01 12:47:48 +0200

    Goal script

---
+++ Quagger/Assets/Scripts/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+    public string Level;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
+
+        if (Level.Length > 0)
+            SceneManager.LoadScene(Level);
+    }
+}
