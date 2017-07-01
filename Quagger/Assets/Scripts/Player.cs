682d9902365257b2bb69c90682a2371bbc23cc0c
Patrick Hipp <patrick.hipp@student.tugraz.at>
2017-07-01 09:46:25 +0200

    Add PlayerSingleton to player

--- Quagger/Assets/Scripts/Player.cs
+++ Quagger/Assets/Scripts/Player.cs
@@ -9,29 +9,75 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        PlayerSingleton.getInstance().reset();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        PlayerSingleton.getInstance().incrementTimer(Time.deltaTime);
 	}
 
     private void FixedUpdate()
     {
+        Vector2 move = rigid.position;
+
         if (Input.GetAxis("Horizontal") != 0)
-        {
-            Vector2 oldPos = rigid.position;
-            oldPos.x += Input.GetAxis("Horizontal") * speed;
-            rigid.position = oldPos;
-            //rigid.MovePosition(new Vector2(Input.GetAxis("Horizontal"), 0));
-        }
+            move.x += Input.GetAxis("Horizontal") * speed;
+            
         if (Input.GetAxis("Vertical") != 0)
-        {
-            Vector2 oldPos = rigid.position;
-            oldPos.y += Input.GetAxis("Vertical") * speed;
-            rigid.position = oldPos;
-        }
+            move.y += Input.GetAxis("Vertical") * speed;
+
+        rigid.position = move;
+    }
+
+
+}
+
+public class PlayerSingleton
+{
+
+    public float time;
+    public int hearts;
+
+    private static PlayerSingleton instance;
+
+    public static PlayerSingleton getInstance()
+    {
+        if (instance == null)
+            instance = new PlayerSingleton();
+        return instance;
+    }
+
+    private PlayerSingleton()
+    {
+        time = 0.0f;
+        hearts = 3;
+    }
+
+    public void reset()
+    {
+        time = 0.0f;
+        hearts = 3;
+    }
+
+    public void lose1Heart()
+    {
+        hearts = Mathf.Max(hearts - 1, 0);
+    }
+
+    // pass deltaTime here 
+    public void incrementTimer(float amount)
+    {
+        time += amount;
+    }
+
+    public string displayTime()
+    {
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        int hundredths = Mathf.FloorToInt((time * 100) % 100);
+
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, hundredths);
     }
 
 }
