743aad4dd9845c25c8545e323e9e8cce1eef50b0
Patrick Hipp <patrick.hipp@student.tugraz.at>
2017-07-01 11:16:19 +0200

    Add movement facing and sprite change based on water

--- Quagger/Assets/Scripts/Player.cs
+++ Quagger/Assets/Scripts/Player.cs
@@ -8,38 +8,65 @@ public class Player : MonoBehaviour {
 
     public Rigidbody2D rigid;
     public float speed = 1;
+    public Sprite standing;
+    public Sprite swimming;
 
     private Text timer;
     private Text hearts;
+    private SpriteRenderer sprite;
+    private bool in_water = false;
+
 
 	// Use this for initialization
 	void Start () {
         PlayerSingleton.getInstance().reset();
         timer = GameObject.Find("Timer").GetComponent<Text>();
         hearts = GameObject.Find("Hearts").GetComponent<Text>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (PlayerSingleton.getInstance().hearts == 0)
+            SceneManager.LoadScene("Gameover");
+
         PlayerSingleton.getInstance().incrementTimer(Time.deltaTime);
         timer.text = PlayerSingleton.getInstance().displayTime();
         hearts.text = new System.String('l', PlayerSingleton.getInstance().hearts);
 
-        if (PlayerSingleton.getInstance().hearts == 0)
-            SceneManager.LoadScene("Gameover");
+        if (in_water)
+            sprite.sprite = swimming;
+        else
+            sprite.sprite = standing;
+
+        Vector2 moveDirection = rigid.velocity;
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            Debug.Log(angle);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    public void setInWater(bool b)
+    {
+        in_water = b;
     }
 
     private void FixedUpdate()
     {
-        Vector2 move = rigid.position;
+        Vector2 move = new Vector2(0,0);
 
-        if (Input.GetAxis("Horizontal") != 0)
-            move.x += Input.GetAxis("Horizontal") * speed;
-            
-        if (Input.GetAxis("Vertical") != 0)
-            move.y += Input.GetAxis("Vertical") * speed;
+        move.x = Input.GetAxis("Horizontal") * speed;
+        move.y = Input.GetAxis("Vertical") * speed;
 
-        rigid.position = move;
+        if (move != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(move.y, move.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        rigid.position += move;
     }
 
 
---
+++ Quagger/Assets/Scripts/Water.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
+
+        collision.GetComponent<Player>().setInWater(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
+
+        collision.GetComponent<Player>().setInWater(false);
+    }
+}
