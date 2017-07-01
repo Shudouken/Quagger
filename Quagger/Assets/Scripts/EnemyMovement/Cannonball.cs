7ec70be3e19b5f87ba0e1d02ba6577051e2ade7f
stefanreichenauer <stefan-reichenauer@gmx.at>
2017-07-01 11:46:51 +0200

    Added Cannon Spawner

---
+++ Quagger/Assets/Scripts/EnemyMovement/Cannonball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Cannonball : MonoBehaviour {
+
+    Rigidbody2D rigid;
+    //float speed = 1;
+
+    float verticalDirection = 0;
+    float horizontalDirection = 0;
+    Vector3 cannonPosition;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    public void Construct(float vertical, float horizontal, Vector3 position)
+    {
+        verticalDirection = vertical;
+        horizontalDirection = horizontal;
+        cannonPosition = position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 oldPos = rigid.position;
+        oldPos.x += horizontalDirection;
+        oldPos.y += verticalDirection;
+        rigid.position = oldPos;
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        //Destroy(gameObject);
+        transform.position = cannonPosition;
+    }
+
+
+}
